3ba6c6f727ad2f2c1b35d2a60bc4b2ea28ed6f41
Noah Brode <gritlabs1@gmail.com>
2025-07-13 01:42:38 -0400

    Fix hello-world app fetch

--- docs/pages/_static/apps/hello-world/hello-world.js
+++ docs/pages/_static/apps/hello-world/hello-world.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const msgEl = document.getElementById('message');
     fetch('https://www.gritlabs.net/api/hello')
-        .then(res => res.json())
-        .then(data => {
-            const text = typeof data === 'object' && 'message' in data ? data.message : JSON.stringify(data);
+        .then(res => res.text())
+        .then(text => {
             msgEl.textContent = text;
         })
         .catch(err => {
