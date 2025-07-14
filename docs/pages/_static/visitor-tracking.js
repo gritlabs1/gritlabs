59c33e504dc2d38a05bb7eaf8c33fc6ace468c5a
Noah Brode <gritlabs1@gmail.com>
2025-07-13 23:31:21 -0400

    Add absolute path

--- docs/pages/_static/visitor-tracking.js
+++ docs/pages/_static/visitor-tracking.js
@@ -41,7 +41,7 @@
     isBot: isLikelyBot()
   };
 
-  fetch('users/visitors/upsert', {
+  fetch('/users/visitors/upsert', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(payload)
